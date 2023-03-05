bcf116ddcc1a97317fd0726efeb011c6145d6c15
Dean <dean.rodgers5@gmail.com>
2023-03-05 21:59:23 +0000

    added javadocs

--- src/sml/RegisterName.java
+++ src/sml/RegisterName.java
@@ -1,5 +1,9 @@
 package sml;
 
+/**
+ * Interface to name registers in the Simple Machine Language (SML).
+ * It should be implemented using Enum for each register name.
+ */
 public interface RegisterName {
     String name();
 }
