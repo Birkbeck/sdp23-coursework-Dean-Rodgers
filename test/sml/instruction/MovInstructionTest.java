c5d95b0ea7fe55635583f595d4690f730f356105
Dean <dean.rodgers5@gmail.com>
2023-03-05 18:25:41 +0000

    added new tests

--- test/sml/instruction/MovInstructionTest.java
+++ test/sml/instruction/MovInstructionTest.java
@@ -8,7 +8,7 @@ import sml.Instruction;
 import sml.Machine;
 import sml.Registers;
 
-import static sml.Registers.Register.EAX;
+import static sml.Registers.Register.*;
 
 public class MovInstructionTest {
     private Machine machine;
@@ -28,6 +28,27 @@ public class MovInstructionTest {
         Instruction instruction = new MovInstruction(null, EAX, 145);
         instruction.execute(machine);
         Assertions.assertEquals(145, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void testToString(){
+        Instruction instruction = new MovInstruction("f5", ECX, 10);
+        Assertions.assertEquals(instruction.toString(), "f5: mov ECX 10");
+
+    }
+
+    @Test
+    void testEquals() {
+        Instruction instruction1 = new MovInstruction(null, ECX, 10);
+        Instruction instruction2 = new MovInstruction(null, ECX, 10);
+        Assertions.assertTrue(instruction1.equals(instruction2));
+    }
+
+    @Test
+    void testEqualsTwo() {
+        Instruction instruction1 = new MovInstruction(null, ECX, 10);
+        Instruction instruction2 = new MovInstruction(null, EBX, 10);
+        Assertions.assertFalse(instruction1.equals(instruction2));
     }
 
 
