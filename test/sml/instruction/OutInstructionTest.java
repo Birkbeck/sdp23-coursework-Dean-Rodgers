3847b62da459be4e2b59f4d00d9f797c958ba4d7
Dean <dean.rodgers5@gmail.com>
2023-03-05 18:40:49 +0000

    new tests

--- test/sml/instruction/OutInstructionTest.java
+++ test/sml/instruction/OutInstructionTest.java
@@ -11,7 +11,8 @@ import sml.Registers;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static sml.Registers.Register.EAX;
+import static sml.Registers.Register.*;
+import static sml.Registers.Register.EBX;
 
 
 public class OutInstructionTest {
@@ -43,6 +44,44 @@ public class OutInstructionTest {
         instruction.execute(machine);
         Assertions.assertEquals(String.valueOf(5), outContent.toString().trim());
     }
+
+    @Test
+    void executeValidTwo() {
+        registers.set(ECX, 60);
+        Instruction instruction = new OutInstruction(null, ECX);
+        instruction.execute(machine);
+        Assertions.assertEquals(String.valueOf(60), outContent.toString().trim());
+    }
+
+    @Test
+    void executeValidThree() {
+        registers.set(ECX, 60);
+        Instruction instruction = new OutInstruction(null, ECX);
+        instruction.execute(machine);
+        Assertions.assertNotEquals(String.valueOf(80), outContent.toString().trim());
+    }
+
+    @Test
+    void testToString(){
+        Instruction instruction = new OutInstruction("f1", EAX);
+        Assertions.assertEquals(instruction.toString(), "f1: out EAX");
+    }
+
+    @Test
+    void testEqualsMethod() {
+        Instruction instruction1 = new OutInstruction(null, EAX);
+        Instruction instruction2 = new OutInstruction(null, EAX);
+        Assertions.assertTrue(instruction1.equals(instruction2));
+    }
+
+    @Test
+    void testEqualsMethodTwo() {
+        Instruction instruction1 = new OutInstruction(null, EBX);
+        Instruction instruction2 = new OutInstruction(null, ECX);
+        Assertions.assertFalse(instruction1.equals(instruction2));
+    }
+
+
 
 
 }
